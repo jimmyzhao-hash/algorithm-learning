4d057da4feb61054ad81fa1c1bc9f4ec5f21b63f
思察 <sicha.zh@alibaba-inc.com>
2021-02-09 16:01:57 +0800

    add validPalindrome

--- src/com/jimmy/Main.java
+++ src/com/jimmy/Main.java
@@ -76,6 +76,26 @@ public class Main {
 
     }
 
+    //valid pladindrome
+
+    public static boolean validPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 	// write your code here
         int [] numbers = new int[] {2,7,11,15,17};
@@ -84,6 +104,7 @@ public class Main {
         int [] rootsqure = squrerootsum(target);
         String words = "leetcode";
         String newWords = reverseVowels(words.toCharArray());
+        validPalindrome("abcba");
         System.out.println(indexpair[0]+indexpair[1]);
         System.out.println(rootsqure[0]+rootsqure[1]);
     }
