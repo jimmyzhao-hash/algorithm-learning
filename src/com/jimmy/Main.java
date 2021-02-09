12eb0eb9ec2cc616b56ccabfcb05aa4b88324ff5
思察 <sicha.zh@alibaba-inc.com>
2021-02-09 15:26:09 +0800

    Initialize the algoritm learning repo

--- src/com/jimmy/Main.java
+++ src/com/jimmy/Main.java
@@ -1,5 +1,8 @@
 package com.jimmy;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Main {
 
     public static int[] twosum(int[] numbers, int target) {
@@ -36,12 +39,51 @@ public class Main {
         return new int []{index1,index2};
     }
 
+    //Reverse Vowel of a string
+
+    private final static HashSet<Character> vowels = new HashSet<>(
+            Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+    public static String reverseVowels(char[] words){
+
+        if (words == null)
+            return null;
+
+        int i =0;
+        int j = words.length -1;
+        char[] newWords = new char[words.length];
+
+        while (i < j){
+            if (!(vowels.contains(words[i]))){
+                newWords[i] = words[i];
+                i++;
+            }
+            else{
+                if(!(vowels.contains(words[j]))){
+                    newWords[j] = words[j];
+                    j--;
+                }
+            }
+
+            newWords[i] = words[j];
+            newWords[j] = words[i];
+            i++;
+            j--;
+
+
+        }
+        return new String(newWords);
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
         int [] numbers = new int[] {2,7,11,15,17};
         int [] indexpair = twosum(numbers, 17);
         int target = 5;
         int [] rootsqure = squrerootsum(target);
+        String words = "leetcode";
+        String newWords = reverseVowels(words.toCharArray());
         System.out.println(indexpair[0]+indexpair[1]);
         System.out.println(rootsqure[0]+rootsqure[1]);
     }
